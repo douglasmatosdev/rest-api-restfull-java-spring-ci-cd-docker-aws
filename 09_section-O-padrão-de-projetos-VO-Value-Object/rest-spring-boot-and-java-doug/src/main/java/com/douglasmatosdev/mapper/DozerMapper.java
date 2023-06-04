fc894ffc083341a1d5c52d9e2a4d1630ecaefb9c
douglasmatosdev <douglasmatosdev@gmail.com>
2023-06-04 13:11:43 -0300

    class 0906 update DozerMapper to ModelMapper

--- 09_section-O-padrão-de-projetos-VO-Value-Object/rest-spring-boot-and-java-doug/src/main/java/com/douglasmatosdev/mapper/DozerMapper.java
+++ 09_section-O-padrão-de-projetos-VO-Value-Object/rest-spring-boot-and-java-doug/src/main/java/com/douglasmatosdev/mapper/DozerMapper.java
@@ -1,13 +1,16 @@
 package com.douglasmatosdev.mapper;
 
-import com.github.dozermapper.core.DozerBeanMapperBuilder;
-import com.github.dozermapper.core.Mapper;
+//import com.github.dozermapper.core.DozerBeanMapperBuilder;
+//import com.github.dozermapper.core.Mapper;
+
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DozerMapper {
-    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+//    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+    private static ModelMapper mapper = new ModelMapper();
 
     public static <Origin, Destination> Destination parseObject(Origin origin, Class<Destination> destination) {
         return mapper.map(origin, destination);
