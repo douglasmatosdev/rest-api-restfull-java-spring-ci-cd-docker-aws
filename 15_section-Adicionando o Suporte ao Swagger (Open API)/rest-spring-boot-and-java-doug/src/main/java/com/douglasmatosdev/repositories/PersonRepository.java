977cfdef19074e5f962fb59eeec4a819911313d9
douglasmatosdev <douglasmatosdev@gmail.com>
2023-06-06 19:20:35 -0300

    add support to Swagger (Open API)

---
+++ 15_section-Adicionando o Suporte ao Swagger (Open API)/rest-spring-boot-and-java-doug/src/main/java/com/douglasmatosdev/repositories/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.douglasmatosdev.repositories;
+
+import com.douglasmatosdev.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
